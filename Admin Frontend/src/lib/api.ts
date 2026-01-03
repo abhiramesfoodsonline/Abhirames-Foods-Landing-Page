76577267cfaa809e432d4268d5bdbb88c176f465
ShyamSrinivasan10 <220701508@rajalakshmi.edu.in>
2026-01-03 21:05:34 +0530

    Environment Variables changed

--- Admin Frontend/src/lib/api.ts
+++ Admin Frontend/src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_BASE_URL;
+
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 
